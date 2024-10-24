25dd8fd20705e7704ab71c475c7cfc4389ab9d21
BERNARDO BONZOUMET JAMES <202102368618@alunos.ibmec.edu.br>
2024-10-24 09:18:28 -0300

    adicionanod teste de api

---
+++ dotnet/SampleWebApi/SampleWebApi/Repository/SampleContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Model;
+using SampleWebApi.Repository.Mapping;
+
+namespace SampleWebApi.Repository
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext(DbContextOptions<SampleContext> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoMapping());
+            modelBuilder.ApplyConfiguration(new FabricanteMapping());
+            modelBuilder.ApplyConfiguration(new VeiculoMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
